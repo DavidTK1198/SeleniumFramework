2c5061d00d046f3b45251ebb07ff3f03834efc1b
DavidTK1198 <ddanielb09@gmail.com>
2023-12-02 21:03:22 -0600

    Update class name
    Update CommonsMethods to  AbstractPage, also remove takeScreenshoot method.

--- src/main/java/dbarrientos/pageObjects/AbstractPage.java
+++ src/main/java/dbarrientos/pageObjects/AbstractPage.java
@@ -1,21 +1,15 @@
 package dbarrientos.pageObjects;
-
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CommonsMethods {
+public class AbstractPage {
 	WebDriver driver;
 
-	public CommonsMethods(WebDriver driver) {
+	public AbstractPage(WebDriver driver) {
 		this.driver = driver;
 	}
 
@@ -36,15 +30,5 @@ public class CommonsMethods {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
 		wait.until(ExpectedConditions.invisibilityOf(element));
 
-	}
-
-	public void takeScreenshoot(String testCaseName) {
-		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		String path = System.getProperty("user.dir") + "\\screenshots\\" + testCaseName + ".png";
-		try {
-			FileUtils.copyFile(src, new File(path));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
